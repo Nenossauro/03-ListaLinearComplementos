29bb641e4a0834470ae8c50a36a1ba714a9c5ac9
Samuel <89140281+Nenossauro@users.noreply.github.com>
2024-09-19 18:24:12 -0300

    bagtulho feito

--- ListaLinear/ListaLinear.cpp
+++ ListaLinear/ListaLinear.cpp
@@ -105,7 +105,7 @@ void inserirElemento()
 
 		if (pos != -1)
 		{
-			cout << "Elemento j� esta na lista" << endl;
+			cout << "Elemento já esta na lista" << endl;
 		}
 		else
 		{
@@ -122,8 +122,27 @@ void inserirElemento()
 
 void excluirElemento()
 {
-
-
+	int valorExclusao;
+	cout << "Digite o elemento que deseja excluir: " << endl;
+	cin >> valorExclusao;
+	int posicao = -1;
+	for (int i = 0; i < nElementos; i++) {
+		if (valorExclusao == lista[i]) {
+			posicao = i;
+			break;
+		}
+	}
+	if (posicao != -1) {
+		for (int i = posicao; i < nElementos; i++) {
+			lista[i] = lista[i + 1];
+		}
+		nElementos--;
+		cout << "O elemento foi excluído." << endl;
+		cout << "Quantidade de elementos atualizada: " << nElementos << endl;
+	}
+	else {
+		cout << "Elemento não encontrado.";
+	}
 }
 
 void buscarElemento()
